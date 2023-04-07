b3bbc5df0a95eeeb93f5656c7a4af0399ba33545
Wei Dai <me@wdai.us>
2023-04-07 13:20:07 -0700

    Update example

--- halo2-base/examples/inner_product.rs
+++ halo2-base/examples/inner_product.rs
@@ -56,8 +56,7 @@ fn main() {
     let mut builder = GateThreadBuilder::new(false);
     inner_prod_bench(builder.main(0), vec![Fr::one(); 5], vec![Fr::one(); 5]);
 
-    let s = builder.main(0).serialize_config();
-    println!("{:?}", Context::<Fr>::from_json(&s));
+    let config_json = builder.main(0).serialize_config();
 
     // Compose the rust witness gen code
     let witness_gen_code = builder.main(0).compose_rust_witness_gen().clone();
@@ -65,43 +64,65 @@ fn main() {
     builder.config(k as usize, Some(20));
     let circuit = GateCircuitBuilder::mock(builder);
 
-
     // Wrap it in a bare file that simply runs the witness gen code
     let raw = format!("{}", quote! {
-        // use halo2_base::halo2_proofs::{
-        //     arithmetic::Field,
-        //     circuit::*,
-        //     dev::MockProver,
-        //     halo2curves::bn256::{Bn256, Fr, G1Affine},
-        //     plonk::*,
-        //     poly::kzg::multiopen::VerifierSHPLONK,
-        //     poly::kzg::strategy::SingleStrategy,
-        //     poly::kzg::{
-        //         commitment::{KZGCommitmentScheme, ParamsKZG},
-        //         multiopen::ProverSHPLONK,
-        //     },
-        //     transcript::{Blake2bRead, TranscriptReadBuffer},
-        //     transcript::{Blake2bWrite, Challenge255, TranscriptWriterBuffer},
-        // };
+        use halo2_base::halo2_proofs::{
+            arithmetic::Field,
+            circuit::*,
+            dev::MockProver,
+            halo2curves::bn256::{Bn256, Fr, G1Affine},
+            plonk::*,
+            poly::kzg::multiopen::VerifierSHPLONK,
+            poly::kzg::strategy::SingleStrategy,
+            poly::kzg::{
+                commitment::{KZGCommitmentScheme, ParamsKZG},
+                multiopen::ProverSHPLONK,
+            },
+            transcript::{Blake2bRead, TranscriptReadBuffer},
+            transcript::{Blake2bWrite, Challenge255, TranscriptWriterBuffer},
+        };
         fn main() {
             let compute = #witness_gen_code;
-            compute();
+            let wires: Vec<Fr> = compute();
+
+            println!("{:?}", wires[wires.len()-1]);
+
+            // let mut builder = GateThreadBuilder::new(true);
+            // let a = (0..5).map(|_| Fr::random(OsRng)).collect_vec();
+            // let b = (0..5).map(|_| Fr::random(OsRng)).collect_vec();
+            // inner_prod_bench(builder.main(0), a, b);
+            // let circuit = GateCircuitBuilder::prover(builder, break_points);
+
+            // let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+            // create_proof::<
+            //     KZGCommitmentScheme<Bn256>,
+            //     ProverSHPLONK<'_, Bn256>,
+            //     Challenge255<G1Affine>,
+            //     _,
+            //     Blake2bWrite<Vec<u8>, G1Affine, Challenge255<_>>,
+            //     _,
+            // >(&params, &pk, &[circuit], &[&[]], OsRng, &mut transcript)
+            // .expect("prover should not fail");
         }
     });
 
     // Write it to `examples/circuit_runtime.rs`
-    let data = RustFmt::default().format_str(raw).unwrap();
+    // let data = RustFmt::default().format_str(raw).unwrap();
 
-    std::fs::write("examples/inner_product_runtime.rs", data).expect("Unable to write file");
+    std::fs::write("examples/inner_product_runtime.rs", raw).expect("Unable to write file");
 
-    // // check the circuit is correct just in case
-    // MockProver::run(k, &circuit, vec![]).unwrap().assert_satisfied();
+    std::fs::write("examples/inner_product_config.json", config_json).expect("Unable to write the config json file");
 
-    // let params = ParamsKZG::<Bn256>::setup(k, OsRng);
-    // let vk = keygen_vk(&params, &circuit).expect("vk should not fail");
-    // let pk = keygen_pk(&params, vk, &circuit).expect("pk should not fail");
+    // check the circuit is correct just in case
+    MockProver::run(k, &circuit, vec![]).unwrap().assert_satisfied();
+
+    let params = ParamsKZG::<Bn256>::setup(k, OsRng);
+    let vk = keygen_vk(&params, &circuit).expect("vk should not fail");
+    let pk = keygen_pk(&params, vk, &circuit).expect("pk should not fail");
 
     // let break_points = circuit.break_points.take();
+
+    // println!("{:?}", break_points);
 
     // let mut builder = GateThreadBuilder::new(true);
     // let a = (0..5).map(|_| Fr::random(OsRng)).collect_vec();
